3cbd64e7b77eeabb32a0fb61635f257e5ed18bab
Typhoon-Z <1802832490@qq.com>
2022-09-27 08:59:07 +0000

    changes

--- 01_variables.dart
+++ 01_variables.dart
@@ -3,27 +3,34 @@ void main(List<String> args)
 {
   default_value();
   late_variables();
+  final_and_const();
 }
 
 //default-value
-viod default_value(){
+void default_value(){
   int lineCount = 0;
   assert(lineCount == 0);
   assert(lineCount != 1);
 }
 
-viod late_variables(){
-
+//late-variables
+void late_variables(){
+  late String description = 'Feijoada!';
+  assert(description == 'Feijoada!');
 }
 
 
-
-late String description;
-
-void main() {
-  description = 'Feijoada!';
-  print(description);
-}
 //Final & Const
-//
-void final_and_const(){}
+void final_and_const(){
+  final name = 'Bob'; // Without a type annotation
+  final String nickname = 'Bobby';
+  const bar = 1000000; // Unit of pressure (dynes/cm2)
+  const double atm = 1.01325 * bar; // Standard atmosphere
+
+  assert(name== 'Bob');
+  assert(nickname=='Bobby');
+  assert(bar ==1000000);
+  assert(atm == 1013250);
+ 
+}
+
