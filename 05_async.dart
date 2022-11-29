d3dd1d1769fff779d8930b72e0bfc6df8406660a
Typhoon-Z <1802832490@qq.com>
2022-11-29 09:09:50 +0000

    messgae

--- 05_async.dart
+++ 05_async.dart
@@ -1,17 +1,22 @@
-import 'dart:async';
+Future<void> printOrderMessage() async {
+  print('Awaiting user order...');
+  var order = await fetchUserOrder();
+  print('Your order is: $order');
+}
 
-void main(List<String> arguments) {
-  // ...
-  FileSystemEntity.isDirectory(/workspace/hello_dart/00_helloworld.dart).then((isDir) {
-    if (isDir) {
-      final startingDir = Directory(00_hellowprld.dart);
-      startingDir.list().listen((entity) {
-        if (entity is File) {
-          searchFile(entity, searchTerms);
-        }
-      });
-    } else {
-      searchFile(File(searchPath), searchTerms);
-    }
-  });
+Future<String> fetchUserOrder() {
+  // Imagine that this function is more complex and slow.
+  return Future.delayed(const Duration(seconds: 4), () => 'Large Latte');
+}
+
+void main() async {
+  countSeconds(4);
+  await printOrderMessage();
+}
+
+// You can ignore this function - it's here to visualize delay time in this example.
+void countSeconds(int s) {
+  for (var i = 1; i <= s; i++) {
+    Future.delayed(Duration(seconds: i), () => print(i));
+  }
 }
